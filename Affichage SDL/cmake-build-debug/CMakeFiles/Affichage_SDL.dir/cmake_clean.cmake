8a8422b627279aa9dcfd8d0c04f59d0cd00358f8
Alexis DOUADY <alexis.douady@viacesi.fr>
2018-11-12 13:52:08 +0100

    SDL 0.0.1
    Création des fichiers du code d'affichage SDL

---
+++ Affichage SDL/cmake-build-debug/CMakeFiles/Affichage_SDL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Affichage_SDL.dir/main.c.obj"
+  "Affichage_SDL.pdb"
+  "Affichage_SDL.exe"
+  "Affichage_SDL.exe.manifest"
+  "libAffichage_SDL.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Affichage_SDL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Affichage SDL/cmake-build-debug/CMakeFiles/Affichage_SDL.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Affichage_SDL.
+# This may be replaced when dependencies are built.
