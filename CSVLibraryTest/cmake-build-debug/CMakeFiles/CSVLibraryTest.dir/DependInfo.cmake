d664f32fa809a4cbe90a0b79b61de33321a850b5
iv230 <lucas.kessayan@gmail.com>
2018-11-12 15:09:36 +0100

    Created CSV librairy project

---
+++ CSVLibraryTest/cmake-build-debug/CMakeFiles/CSVLibraryTest.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Lucas/Documents/GitHub/HeXart/CSVLibraryTest/features/reader.c" "C:/Users/Lucas/Documents/GitHub/HeXart/CSVLibraryTest/cmake-build-debug/CMakeFiles/CSVLibraryTest.dir/features/reader.c.obj"
+  "C:/Users/Lucas/Documents/GitHub/HeXart/CSVLibraryTest/features/remover.c" "C:/Users/Lucas/Documents/GitHub/HeXart/CSVLibraryTest/cmake-build-debug/CMakeFiles/CSVLibraryTest.dir/features/remover.c.obj"
+  "C:/Users/Lucas/Documents/GitHub/HeXart/CSVLibraryTest/features/reseter.c" "C:/Users/Lucas/Documents/GitHub/HeXart/CSVLibraryTest/cmake-build-debug/CMakeFiles/CSVLibraryTest.dir/features/reseter.c.obj"
+  "C:/Users/Lucas/Documents/GitHub/HeXart/CSVLibraryTest/features/writer.c" "C:/Users/Lucas/Documents/GitHub/HeXart/CSVLibraryTest/cmake-build-debug/CMakeFiles/CSVLibraryTest.dir/features/writer.c.obj"
+  "C:/Users/Lucas/Documents/GitHub/HeXart/CSVLibraryTest/main.c" "C:/Users/Lucas/Documents/GitHub/HeXart/CSVLibraryTest/cmake-build-debug/CMakeFiles/CSVLibraryTest.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CSVLibraryTest/cmake-build-debug/CMakeFiles/CSVLibraryTest.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CSVLibraryTest.dir/main.c.obj"
+  "CMakeFiles/CSVLibraryTest.dir/features/reader.c.obj"
+  "CMakeFiles/CSVLibraryTest.dir/features/writer.c.obj"
+  "CMakeFiles/CSVLibraryTest.dir/features/reseter.c.obj"
+  "CMakeFiles/CSVLibraryTest.dir/features/remover.c.obj"
+  "CSVLibraryTest.pdb"
+  "CSVLibraryTest.exe"
+  "CSVLibraryTest.exe.manifest"
+  "libCSVLibraryTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CSVLibraryTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
