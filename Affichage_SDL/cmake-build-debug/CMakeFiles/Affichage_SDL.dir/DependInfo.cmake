a6a214884e1afd3ca738a92e755c67ed5a88d0e7
iv230 <lucas.kessayan@gmail.com>
2018-11-19 10:23:18 +0100

    Updated GitIgnore

---
+++ Affichage_SDL/cmake-build-debug/CMakeFiles/Affichage_SDL.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Alex/Documents/Travaux/C/HeXart/HeXart/Affichage SDL/generator/generator.c" "C:/Users/Alex/Documents/Travaux/C/HeXart/HeXart/Affichage SDL/cmake-build-debug/CMakeFiles/Affichage_SDL.dir/generator/generator.c.obj"
+  "C:/Users/Alex/Documents/Travaux/C/HeXart/HeXart/Affichage SDL/loading/loading.c" "C:/Users/Alex/Documents/Travaux/C/HeXart/HeXart/Affichage SDL/cmake-build-debug/CMakeFiles/Affichage_SDL.dir/loading/loading.c.obj"
+  "C:/Users/Alex/Documents/Travaux/C/HeXart/HeXart/Affichage SDL/main.c" "C:/Users/Alex/Documents/Travaux/C/HeXart/HeXart/Affichage SDL/cmake-build-debug/CMakeFiles/Affichage_SDL.dir/main.c.obj"
+  "C:/Users/Alex/Documents/Travaux/C/HeXart/HeXart/Affichage SDL/menu/menu.c" "C:/Users/Alex/Documents/Travaux/C/HeXart/HeXart/Affichage SDL/cmake-build-debug/CMakeFiles/Affichage_SDL.dir/menu/menu.c.obj"
+  "C:/Users/Alex/Documents/Travaux/C/HeXart/HeXart/Affichage SDL/utils/utils.c" "C:/Users/Alex/Documents/Travaux/C/HeXart/HeXart/Affichage SDL/cmake-build-debug/CMakeFiles/Affichage_SDL.dir/utils/utils.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
